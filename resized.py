6b22ba32fe6cc43ad90a94ac168b347d661beab0
cjg9040 <chl8273@naver.com>
2020-04-13 12:36:27 +0900

    time

--- resized.py
+++ resized.py
@@ -1,6 +1,7 @@
 import boto3
 from PIL import Image, ImageFilter
 import json
+import time
 
 return_bucket_name = 'aug-module'
 
@@ -22,13 +23,23 @@ def augmentation(file_name, image_path):
 
 def handler(event, context):
     records = json.loads(event['Records'][0]['Sns']['Message'])
+    s3_time = 0
+    aug_time = 0
     for record in records['Records']:
         bucket_name = record['s3']['bucket']['name']
         object_path = record['s3']['object']['key']
         tmp = '/tmp/' + object_path
+        s3_start = time.time()
         s3 = boto3.client('s3')
         s3.download_file(bucket_name, object_path, tmp)
+        s3_end = time.time()
+        s3_time += s3_end - s3_start
+        aug_start = time.time()
         ret = augmentation(object_path, tmp)
+        aug_end = time.time()
+        aug_time += aug_end - aug_start
         s3.upload_file(ret, return_bucket_name, ret.split('/')[2])
+    print('s3_time: ', s3_time)
+    print('aug_time: ', aug_time)
     print('resized')
     return 'resized'
